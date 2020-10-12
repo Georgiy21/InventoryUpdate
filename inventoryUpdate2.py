44a543ba06c3f1590d026218efff4eb1f11d40b3
Georgiy21 <georgiy.voropayev@gmail.com>
2020-10-12 16:36:51 -0700

    OOP style

---
+++ inventoryUpdate2.py
@@ -0,0 +1,20 @@
+import csv
+import fnmatch
+import os.path
+from os import path
+from datetime import datetime
+from datetimerange import DateTimeRange
+from datetime import timedelta
+
+
+class Product:
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
